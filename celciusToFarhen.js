c4b4d1ed3f850c7912f791eb4d408a79d01ebe68
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-02-17 20:03:02 +0530

    Completed 'temperateCoverstion' program

---
+++ celciusToFarhen.js
@@ -0,0 +1,9 @@
+function conertToFarhen(value) {
+    return (value * 9 / 5) + 32;
+}
+
+function main() {
+    console.log(conertToFarhen(36))
+}
+
+main();
---
+++ farhenToCelcius.js
@@ -0,0 +1,9 @@
+function conertToCelcius(value) {
+    return (value - 32) * 5 / 9;
+}
+
+function main() {
+    console.log(conertToCelcius(93))
+}
+
+main();
